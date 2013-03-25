ea55b8e7f0c23082238847a644171a36760a7fde
Matheus <matheus@matheus-notebook.(none)>
2013-03-25 05:24:18 -0300

    .txt

--- app/models/accel_data.rb
+++ app/models/accel_data.rb
@@ -2,7 +2,7 @@ class AccelData < ActiveRecord::Base
   attr_accessible :bpm, :measure_time, :x, :y, :z, :time_in_milliseconds
 
   def time_in_milliseconds
-    self.measure_time.strftime("%s")
+    self.measure_time.strftime("%s") + "000"
   end
 
   def time_in_milliseconds=(milliseconds)
