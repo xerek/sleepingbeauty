db8d5a43dda3d0cd6d6c092083bfe9752224fddc
JoaoFelipe <joaofelipenp@gmail.com>
2013-03-27 02:18:59 -0500

    model comments

--- app/models/heart_monitor.rb
+++ app/models/heart_monitor.rb
@@ -1,3 +1,16 @@
+#    heart_monitor.rb
+# Purpose: Mid term demo
+# Author: Matheus Camargo
+#         matheus@go.wustl.edu
+# CSE 467S -- Embedded Computing Systems
+# WUSTL, Spring 2013
+# Date: Mar., 23, 2013
+#
+# Description
+#   Model of MVC architecture for the heart monitor datas
+#   
+# => Not used due sensor problems
+#
 class HeartMonitor < ActiveRecord::Base
   attr_accessible :bpm
 end
--- app/models/light_power.rb
+++ app/models/light_power.rb
@@ -1,3 +1,14 @@
+#    light_power.rb
+# Purpose: Mid term demo
+# Author: Matheus Camargo
+#         matheus@go.wustl.edu
+# CSE 467S -- Embedded Computing Systems
+# WUSTL, Spring 2013
+# Date: Mar., 26, 2013
+#
+# Description
+#   Model of MVC architecture for the light power data
+#
 class LightPower < ActiveRecord::Base
   attr_accessible :on, :time
 
